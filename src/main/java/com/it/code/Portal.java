929a7630098806fb19e1e5ec3ce8e2215efb632e
温建武_微智信业 <wenjw@mvtech.com.cn>
2018-08-03 01:06:59 +0800

    ww

--- src/main/java/com/it/code/Portal.java
+++ src/main/java/com/it/code/Portal.java
@@ -6,9 +6,11 @@ public class Portal {
 		// TODO Auto-generated method stub
 		System.out.println("我是A");
 		int a=2;
-		int b=3;
+		int b=13;
 		int e=6;
 		int g=8;
+		int c=4;
+		int d=5;
 	}
 
 }
